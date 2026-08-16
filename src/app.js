66442e79cc54165b99a49e31398649077e5e0c1b
aayush <ar0671362@gmail.com>
2026-08-16 09:48:56 +0530

    tsting endpoints

--- src/app.js
+++ src/app.js
@@ -2,8 +2,20 @@ import express from "express";
 const app = express();
 
 
-app.use((req,res)=>{
-    res.send("hello from the server");
+// app.use((req,res)=>{
+//     res.send("hello from the server");
+// })
+app.use("/hello/2",(req,res)=>{
+    res.send("hello from the /hello/2");
+})
+app.use("/hello",(req,res)=>{
+    res.send("hello from the /hello");
+})
+
+
+
+app.use("/test",(req,res)=>{
+    res.send("testing.....")
 })
 
 app.listen(3000,()=>{
