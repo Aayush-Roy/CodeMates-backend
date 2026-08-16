cf7e761e2b8a96398911a6a4e9e86065c2d78cbb
aayush <ar0671362@gmail.com>
2026-08-16 09:09:01 +0530

    created red handlers

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("starting...");
+import express from "express";
+const app = express();
+
+
+app.use((req,res)=>{
+    res.send("hello from the server");
+})
+
+app.listen(3000,()=>{
+    console.log("Server is listening on port 3000....")
+})
