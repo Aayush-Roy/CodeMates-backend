8e461998c71e3b3cb31a26b02bf9527655192b3c
aayush <ar0671362@gmail.com>
2026-08-16 15:07:26 +0530

    request handlers

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 import express from "express";
 const app = express();
 
-let user = [];
-
-app.get("/user",(req,res)=>{
-    return res.json(user);
-})
-
-app.post("/user",(req,res)=>{
-    user.push({name:"Aayush",email:"a@gmail.com"});
-    return res.json(user);
-})
+app.use("/user",(req,res, next)=>{
+    console.log("Response!");
+    // res.send("req 1");
+    next();
+},(req,res)=>{
+    console.log("response 2");
+    res.send("req 2")
+});
 
 app.listen(3000,()=>{
     console.log("Server is listening on port 3000....")
