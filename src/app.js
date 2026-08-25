b81cb0df41ee9f3fb17e0eb1dfbbfcbb44c2fe4a
aayush <ar0671362@gmail.com>
2026-08-25 22:17:59 +0530

    some api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from "express";
 import { connectDB } from "./config/database.js";
+import { userModel } from "./models/user.js";
 const app = express();
+
+
+
+app.post("/signup",async(req,res)=>{
+    const newUser = await userModel({
+        firstName:"Aayush",
+        lastName:"Roy",
+        age:21,
+        email:"aayush@gmail.com",
+        password:"12345",
+    })
+    newUser.save();
+    res.send("User Created", newUser)
+})
 
 connectDB().then(()=>{
     console.log("DB connection established");
