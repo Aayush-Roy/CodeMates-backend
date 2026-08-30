d84b361036fa6d57d2584446fd6c6cf6a29eae9a
aayush <ar0671362@gmail.com>
2026-08-30 14:22:03 +0530

    bcrypt

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,13 +1,15 @@
-import validator from "validator"
-export const validateSignUpData = (req)=>{
-    const {firstName, lastName, email, password} = req.body;
-    if(!firstName || !lastName){
-        throw new Error("First Name and Last Name is Required")
+import validator from "validator";
+
+export const validateSignUpData = (req) => {
+    const { firstName, lastname, email, password } = req.body;
+
+    if (!firstName || !lastname) {
+        throw new Error("First Name and Last Name are Required");
     }
-    else if(validator.isEmail(email)){
+    else if (!validator.isEmail(email)) {
         throw new Error("Email is Not Valid!");
     }
-    else if(!validator.isStrongPassword(password)){
-        throw new Error("Please enter a strong password!")
+    else if (!validator.isStrongPassword(password)) {
+        throw new Error("Please enter a strong password!");
     }
-}
+};
