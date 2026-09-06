d93cab9fd49126dcf0487d680a790ebac2d257ed
aayush <ar0671362@gmail.com>
2026-09-06 10:04:13 +0530

    logout api

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -52,4 +52,12 @@ router.post("/login", async (req,res)=>{
         }
 })
 
+
+router.post("/logout", (req,res)=>{
+    res.cookie("token", null, {
+        expires:new Date(Date.now());
+    })
+    res.send("Logout Success")
+})
+
 export default router;
