345ae4a40ecbfaf27063cbe142fbf16a60050709
aayush <ar0671362@gmail.com>
2026-09-08 22:37:21 +0530

    connection req api

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -3,17 +3,23 @@ import mongoose from "mongoose";
 const connectionSchema = mongoose.Schema({
     fromUserId:{
         type:mongoose.Schema.Types.ObjectId,
+        required:true,
     },
     toUserId:{
-        type:mongoose.Schema.Types.ObjectId
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+
     },
     status:{
         type:String,
+        required:true,
         enum:{
             values:["ignore","interested","accepted","rejected"],
             message:`{VALUE} is incorrect type`
         }
     }
 },{
-    timestamps:true
+    timestamps:true,
 })
+
+export const Connection = mongoose.model("Connection", connectionSchema);
--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -1,11 +1,37 @@
 import { Router } from "express";
 import { userAuth } from "../middleware/adminAuth.js";
+import { Connection } from "../models/connectionRequest.js";
 const router = Router();
 
-router.post("/sendConnectionRequest", userAuth, async(req,res)=>{
-    const user = req.user;
-    console.log("connection req sent");
-    res.send(`${user.firstName} sent a connection request`);
+
+// router.post("/sendConnectionRequest", userAuth, async(req,res)=>{
+//     const user = req.user;
+//     console.log("connection req sent");
+//     res.send(`${user.firstName} sent a connection request`);
+// })
+
+router.post("/request/send/:status/:toUserId", userAuth, async(req,res)=>{
+    try{
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        const connectionRequest = new Connection({
+            fromUserId,
+            toUserId,
+            status,
+        })
+
+        const data = await connectionRequest.save();
+        res.json({
+            message:"connection req sent!!",
+            data,
+        })
+
+    }catch(err){
+        res.status(400).send("Error", err.message);
+    }
+    
 })
 
 export default router;
