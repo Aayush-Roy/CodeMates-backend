ab4a35ce08cebcd4ffed843cfd9a2a8f102aefe5
aayush <ar0671362@gmail.com>
2026-09-24 22:23:44 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -6,9 +6,15 @@ import authRoute from "./routes/authRoutes.js";
 import profileRoute from "./routes/profileRoutes.js";
 import requestRouter from "./routes/requestRouter.js";
 import userRouter from "./routes/userRoutes.js";
+import cors from "cors"
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true,
+}))
+
 
 app.use("/", authRoute);
 app.use("/", profileRoute);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -41,7 +41,7 @@ router.post("/login", async (req,res)=>{
             if (isPasswordValid) {
             const token = await user.getJWT();
             res.cookie("token",token, {expires:new Date(Date.now()+8*360000)});
-            res.send("Login Successful!!");
+            res.send(user);
             }else{
                 throw new Error("Invalid Credentials");
             }
